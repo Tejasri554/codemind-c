9e0f487299d76c2f95f41258e20131bd3b2a64cc
Tejasri554 <97598370+Tejasri554@users.noreply.github.com>
2022-02-25 22:31:10 +0530

    #include<stdio.h> int main() {     char str[]="Hello Technicalhub",temp;     char string[100000];
        scanf("%[^
    ]s",string);
    
        printf("%s
    %s",str,string);
        return 0;
    }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char str[]="Hello Technicalhub",temp;
+    char string[100000];
+    
+    scanf("%[^
+]s",string);
+
+    printf("%s
+%s",str,string);
+    return 0;
+}
