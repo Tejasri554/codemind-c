d66e6ab4b4cdd84bf9b307bd363d6b449a33b6e6
Tejasri554 <97598370+Tejasri554@users.noreply.github.com>
2022-06-28 16:45:06 +0530

    #include<stdio.h> int main() {     int n,i,j,k,p1=0,p2=0,c=0;     scanf("%d",&k);     while(k>0)     {         scanf("%d",&n);         for(i=n;i>2;i--)         {             c=0;             for(j=2;j<i;j++)             {                 if(i%j==0)                 {                     c=1;                     break;                 }             }             if(c==0)             {                 p1=i;                 break;             }         }         for(i=n;i<n+100;i++)         {             c=0;             for(j=2;j<i;j++)             {                 if(i%j==0)                 {                     c=1;                     break;                 }             }             if(c==0)             {                 p2=i;                 break;             }         }         if(n-p1<=p2-n)         {             printf("%d ",p1);         }         else         {             printf("%d ",p2);         }         k--;     }     return 0; }

---
+++ Nearest_Prime.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k,p1=0,p2=0,c=0;
+    scanf("%d",&k);
+    while(k>0)
+    {
+        scanf("%d",&n);
+        for(i=n;i>2;i--)
+        {
+            c=0;
+            for(j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    c=1;
+                    break;
+                }
+            }
+            if(c==0)
+            {
+                p1=i;
+                break;
+            }
+        }
+        for(i=n;i<n+100;i++)
+        {
+            c=0;
+            for(j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    c=1;
+                    break;
+                }
+            }
+            if(c==0)
+            {
+                p2=i;
+                break;
+            }
+        }
+        if(n-p1<=p2-n)
+        {
+            printf("%d
+",p1);
+        }
+        else
+        {
+            printf("%d
+",p2);
+        }
+        k--;
+    }
+    return 0;
+}
